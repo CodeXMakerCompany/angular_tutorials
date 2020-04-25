35413e5ad5545fc19ce6d29629b42e848b02c555
codexmaker <codexmaker12@gmail.com>
2020-04-25 09:11:47 -0500

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,36 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { log } from 'util';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   title = 'tips';
+
+  ngOnInit() {
+   
+    //Clase en typescript
+    class Usuario {
+      //Función que se ejecuta cada vez que se instancia la clase
+      constructor(
+        public nombre           : string,
+        public apellidos        : string,
+        public nickname         : string,
+        public edad             : string,
+        public genero           : string,
+        public email            : string,
+        public password         : string
+        ){}
+    }
+
+    const new_user = new Usuario('Sam','Apellidos', 'codexmaker',
+                                 '24', 'M', 'codex@gmail.com', '123');
+
+    console.log(new_user);
+
+
+   }
+
 }
